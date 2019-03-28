55a70b9fa682689c528f39932bce4a5c0260054f
Marc Davies and Michael Larragueta <marc@>
2019-03-28 10:54:22 -0700

    Add scripts and css to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,27 @@
 $(function() {
-      $
-
+  var age = parseInt(prompt("Enter your age"));
+    if(age > 0 && age < 10){
+      $("#easy").removeClass();
+      $("#easy").addClass("highlight");
+    }else if (age >= 10 && age <= 15){
+            $("#easy, #medium").removeClass();
+            $("#easy, #medium").addClass("highlight");
+          }else if (age >= 16 && age <= 99){
+                  $("#easy, #medium, #hard").removeClass();
+                  $("#easy, #medium, #hard").addClass("highlight");
+                }else alert("Youre too old, go away");
 });
+
+
+
+    //shows only available
+    //$(function() {
+    //var age = parseInt(prompt("Enter your age"));
+    //if(age > 0 && age < 10){
+    // if(age > 0 && age < 10){
+    //   $("#easy").show();
+    // }else if (age >= 10 && age <= 15){
+    //   $("#easy, #medium").show();
+    // }else if (age >= 16 && age <= 99){
+    //   $("#easy, #medium, #hard").show();
+    // }else alert("Youre too old, go away");
